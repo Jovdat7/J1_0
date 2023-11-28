7f2a0afb34a68ace9c2c6477f02daf6cc0315a1a
jovdat12 <142516173+jovdat12@users.noreply.github.com>
2023-11-28 18:47:17 +0400

    :wq

--- app/app/settings.py
+++ app/app/settings.py
@@ -84,23 +84,23 @@ WSGI_APPLICATION = "app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#         "PORT": "5432"
-#     }
-# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "jovdat_db",
-        "USER": "jovdat_user",
-        "PASSWORD": "d8y46KEACjMtdpeRf5TaMjCod1gtBlh2A8XfFs0zrga7iMaeNuRf5SL95hvbEhy7w",
-        "HOST": "localhost",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        "PORT": "5432"
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "jovdat_db",
+#         "USER": "jovdat_user",
+#         "PASSWORD": "d8y46KEACjMtdpeRf5TaMjCod1gtBlh2A8XfFs0zrga7iMaeNuRf5SL95hvbEhy7w",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
